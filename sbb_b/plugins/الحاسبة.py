f74860510561a795277f3b6c4cc0392de460e457
abdullah1shy <101516117+abdullah1shy@users.noreply.github.com>
2022-06-17 14:56:48 +0300

    Update الحاسبة.py

--- sbb_b/plugins/الحاسبة.py
+++ sbb_b/plugins/الحاسبة.py
@@ -41,7 +41,7 @@ lst.append([Button.inline("=", data="calc=")])
 @sbb_b.ar_cmd(pattern="الحاسبة(?:\s|$)([\s\S]*)")
 async def icalc(e):
     if e.client._bot:
-        return await e.reply("**الحاسبة العلمية لسورس جمثون\n @sbb_b**", buttons=lst)
+        return await e.reply("**الحاسبة العلمية بيكاثون\n @Pegathon **", buttons=lst)
     results = await e.client.inline_query(Config.TG_BOT_USERNAME, "calc")
     await results[0].click(e.chat_id, silent=True, hide_via=True)
     await e.delete()
@@ -57,7 +57,7 @@ async def inlinecalc(event):
     ) and string == "calc":
         event.builder
         calc = event.builder.article(
-            "Calc", text="**الحاسبة العلمية لسورس جمثون\n @sbb_b**", buttons=lst
+            "Calc", text="**الحاسبة العلمية لسورس بيكاثون @sbb_b**", buttons=lst
         )
         await event.answer([calc])
 
@@ -73,13 +73,13 @@ async def _(e):  # sourcery no-metrics
         if CALC.get(user):
             CALC.pop(user)
         await e.edit(
-            "**الحاسبة العلمية لسورس جمثون\n @Pegathon**",
+            "**الحاسبة العلمية لسورس بيكاثون\n @Pegathon**",
             buttons=[Button.inline("افتح مره اخرى", data="recalc")],
         )
     elif x == "C":
         if CALC.get(user):
             CALC.pop(user)
-        await e.answer("تم الحذف")
+        await e.answer(" ✅ تم الحذف")
     elif x == "⌫":
         if CALC.get(user):
             get = CALC[user]
@@ -157,4 +157,4 @@ async def _(e):
     tultd = [Button.inline(f"{x}", data=f"calc{x}") for x in m]
     lst = list(zip(tultd[::4], tultd[1::4], tultd[2::4], tultd[3::4]))
     lst.append([Button.inline("=", data="calc=")])
-    await e.edit("**الحاسبة العلمية لسورس جمثون\n @Pegathon**", buttons=lst)
+    await e.edit("**الحاسبة العلمية لسورس بيكاثون\n @Pegathon**", buttons=lst)
